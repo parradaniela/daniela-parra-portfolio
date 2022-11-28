0f0840217076c09d13f8c46cbb80c8fce0f83fbd
DanielaParra <dparralp396@gmail.com>
2022-11-27 20:02:41 -0500

    Done all media queries, still room to improve

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,16 @@
 const portfolioApp = {};
 
+portfolioApp.tabletMediaQuery = window.matchMedia('(max-width: 768px)');
+
 portfolioApp.init = () => {
     portfolioApp.toggleTabs();
-    // portfolioApp.toggleSrOnly();
+    portfolioApp.landingPageContact();
+    portfolioApp.attachNewOffsiteLinks(portfolioApp.tabletMediaQuery);
 }
 
-// Credit to the FRONTRU - Web Tutorials video on Youtube "Simple Tabs using HTML, CSS & JavaScript" for the easy to follow logic used in the tab method below. Link: https://youtu.be/3d8AwNea4lM 
+// Credit to the FRONTRU - Web Tutorials video on Youtube "Simple Tabs using HTML, CSS & JavaScript" for the easy to follow logic used in the tab method below. Link: https://youtu.be/3d8AwNea4lM
 
+// Method to handle all tab buttons in the header to toggle to the correct content section
 portfolioApp.toggleTabs = () => {
     const tabButtons = document.querySelectorAll('.tabs-toggle');
     const tabContents = document.querySelectorAll('.tabs-body-content');
@@ -22,24 +26,43 @@ portfolioApp.toggleTabs = () => {
     });
 }
 
-// portfolioApp.toggleSrOnly = () => {
-//     const offsiteLinks = document.querySelectorAll('.offsite-link');
-//     // console.log(offsiteLinks);
-//     offsiteLinks.forEach((link) => {
-//         link.addEventListener('mouseenter', (event) => {
-//             const srOnlyParagraph = event.target.firstElementChild.children[1];
-//             srOnlyParagraph.classList.remove('sr-only');
-//         })
-//         link.addEventListener('mouseout', (event) => {
-//             const srOnlyParagraph = event.originalTarget.children[1];
-//             srOnlyParagraph.classList.add('sr-only');
-//         })
-//     })
-// }
+// Method that makes the landing page contact buttont to toggle to the contact page
+portfolioApp.landingPageContact = () => {
+    // Grabs the landing page contact button, the toggle button in the navigation, and the contact section by their IDs
+    const homeContactBtn = document.querySelector('#home-contact-btn');
+    const contactToggle = document.querySelector('#contact-toggle')
+    const contactSection = document.querySelector('#contact');
+    homeContactBtn.addEventListener('click', () => {
+        // Same as the toggleTabs method, removes the active class from all buttons and sections
+        const tabButtons = document.querySelectorAll('.tabs-toggle');
+        const tabContents = document.querySelectorAll('.tabs-body-content');
+        tabButtons.forEach(button => button.classList.remove('is-active'));
+        tabContents.forEach(contentSection => contentSection.classList.remove('is-active'));
+        // This time, adds the active class specifically to the contact toggle and section
+        contactToggle.classList.add('is-active');
+        contactSection.classList.add('is-active');
+    });
+}
 
-//TODO: I suspect that in order to get the landing page button to connect to the contact me tab, it will need its own function
-    // attach event listener to the button
-    // on click, removes is-active class from all tab content and buttons
-    // then applies the class specifically to the contact me button and content - maybe use ID for this after all
+portfolioApp.attachNewOffsiteLinks = () => {
+    // Define media query
+    portfolioApp.tabletMediaQuery.addEventListener("change", () => {
+        const newGithubDiv = document.createElement('div');
+        const newLinkedInDiv = document.createElement('div');
+        const offsiteLinkAnchors = document.querySelectorAll('.offsite-link');
+        const landingPage = document.querySelector('.landing')
+        console.log(offsiteLinkAnchors);
+        console.log(offsiteLinkAnchors[0]);
+        // offsiteLinkAnchors.forEach((anchor) => {
+        //     console.log(anchor);
+        //     newLinkedInDiv.innerHTML = anchor[0].innerHTML;
+        //     newGithubDiv.innerHTML = anchor[1].innerHTML;
+        //     console.log(newLinkedInDiv, newGithubDiv);
+        //     // landingPage.append(newDiv);
+        // })
+        // newDiv.append(offsiteLinkAnchors);
+
+    })
+}
 
 portfolioApp.init();
