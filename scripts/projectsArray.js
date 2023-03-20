e0e1206e26cc3cb4998d43687f25d1496d1205a9
DanielaParra <dparralp396@gmail.com>
2023-03-20 09:08:29 -0400

    Re-adding LIATOA to project showcase, commenting out Funnylibs

--- scripts/projectsArray.js
+++ scripts/projectsArray.js
@@ -1,23 +1,24 @@
 export const projectsArray = [
     {
+        heading: "Love in a Time of Allergies",
+        image: "./assets/liatoa-screenshot.png",
+        mobileImg: "./assets/love-allergies-mobile.png",
+        tech: "React.js, TypeScript, CSS, Firebase",
+        liveURL: "https://liatoa.netlify.app/",
+        repoURL: "https://github.com/parradaniela/loveInATimeOfAllergiesV2",
+        desc: `A React.js TypeScript app that allows the user to save dinner parties with guest names and dietary restrictions to a Firebase database, then connects to a REST API to source 20 dinner recipes filtered by the whole group's dietary restrictions. `
+    },
+
+    {
         heading: "DnD 5e Character Race App",
         image: "./assets/dnd-app.png",
         mobileImg: "./assets/dnd-app-mobile.png",
         tech: "React.js, CSS",
         liveURL: "https://wondrous-fudge-759bc2.netlify.app/",
         repoURL: "https://github.com/parradaniela/dnd-5e-character-race-app",
-        desc: "React.js App that allows the user to select a standard race from the 5th edition of Dungeons and Dragons using a dropdown menu, then displays information about that race by fetching data from a RESTful API to populate a number of tabs. Uses the React Context API to manage prop data across the app structure."
+        desc: "React.js App that allows the user to select a standard race from the 5th edition of Dungeons and Dragons using a dropdown menu, then displays information about that race by fetching data from a REST API to populate a number of tabs."
     },
 
-    // {
-    //     heading: "Love in a Time of Allergies",
-    //     image: "./assets/love-allergies.png",
-    //     mobileImg: "./assets/love-allergies-mobile.png",
-    //     tech: "React.js, CSS, Firebase",
-    //     liveURL: "https://love-in-a-time-of-allergies.netlify.app/",
-    //     repoURL: "https://github.com/LoveInATimeOfAllergies/loveInATimeOfAllergies",
-    //     desc: `A React.js app that allows the user to save dinner parties with guest names and dietary restrictions to a Firebase database, then connects to a RESTFUL API to source 20 dinner recipes that avoid all of the group's dietary restrictions. Group project designed with <a href="https://github.com/leohon">Leo Hon</a>, <a href="https://github.com/Gogulan12">Gogulan Ravichandran</a>, and <a href="https://github.com/LucPasq">Lucas Pasqualoni</a>.`
-    // },
 
     {
         heading: "Sanctuary Helpers",
@@ -39,14 +40,14 @@ export const projectsArray = [
         desc: `Vanilla JS application that uses the AccuWeather API to pull current forecast data for Toronto, and any city within Ontario entered by the user. Pair project with <a href="https://radoyko.com/">Radojko Radulovic</a>.`
     },
 
-    {
-        heading: "FunnyLibs!",
-        image: "./assets/funnylibs.png",
-        mobileImg: "./assets/funnylibs-mobile.png",
-        tech: "React.js, CSS",
-        liveURL: "https://superlative-pony-40db95.netlify.app/",
-        repoURL: "https://github.com/madLibsClone/FunnyLibs/tree/main",
-        desc: `A React.js mad libs app designed together with <a href="https://www.tylergeorge.ca/">Tyler George</a>. The app chooses from three random blocks of text and allows the user to replace some words in the text through a form. Uses React Router to navigate between different pages. Check back for updates, we are working on adding more functionality!`,
-        inProgress: true
-    }
+    // {
+    //     heading: "FunnyLibs!",
+    //     image: "./assets/funnylibs.png",
+    //     mobileImg: "./assets/funnylibs-mobile.png",
+    //     tech: "React.js, CSS",
+    //     liveURL: "https://superlative-pony-40db95.netlify.app/",
+    //     repoURL: "https://github.com/madLibsClone/FunnyLibs/tree/main",
+    //     desc: `A React.js mad libs app designed together with <a href="https://www.tylergeorge.ca/">Tyler George</a>. The app chooses from three random blocks of text and allows the user to replace some words in the text through a form. Uses React Router to navigate between different pages. Check back for updates, we are working on adding more functionality!`,
+    //     inProgress: true
+    // }
 ];
