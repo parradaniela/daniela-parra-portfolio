2f930987323718deaf66075c5f90555543f83d78
DanielaParra <dparralp396@gmail.com>
2023-05-02 17:13:54 -0400

    Added Jobi project to showcase

--- scripts/projectsArray.js
+++ scripts/projectsArray.js
@@ -1,53 +1,51 @@
 export const projectsArray = [
-    {
-        heading: "Love in a Time of Allergies",
-        image: "./assets/liatoa-screenshot.png",
-        mobileImg: "./assets/love-allergies-mobile.png",
-        tech: "React.js, TypeScript, CSS, Firebase",
-        liveURL: "https://liatoa.netlify.app/",
-        repoURL: "https://github.com/parradaniela/loveInATimeOfAllergiesV2",
-        desc: `A React.js TypeScript app that allows the user to save dinner parties with guest names and dietary restrictions to a Firebase database, then connects to a REST API to source 20 dinner recipes filtered by the whole group's dietary restrictions. `
-    },
+  {
+    heading: "Jobi Job Board",
+    image: "./assets/jobi-app.png",
+    mobileImg: "./assets/jobi-mobile.png",
+    tech: "React.js, TypeScript, Firebase Firestore, TailwindCSS",
+    liveURL: "https://jobi-board.netlify.app",
+    repoURL: "https://github.com/parradaniela/jobi-jobboard",
+    desc: "My most recent and currently in-progress project! It aims to be a multi-page, responsive and accessible website conversion from a Figma mockup that will integrate with a Firebase Firestore to provide functionality. Designed mobile-first, using reusable UI components and managing state through the React Context API.",
+    inProgress: true,
+  },
+  {
+    heading: "Love in a Time of Allergies",
+    image: "./assets/liatoa-screenshot.png",
+    mobileImg: "./assets/love-allergies-mobile.png",
+    tech: "React.js, TypeScript, CSS, Firebase",
+    liveURL: "https://liatoa.netlify.app/",
+    repoURL: "https://github.com/parradaniela/loveInATimeOfAllergiesV2",
+    desc: `A React.js TypeScript app that allows the user to save dinner parties with guest names and dietary restrictions to a Firebase database, then connects to a REST API to source 20 dinner recipes filtered by the whole group's dietary restrictions. `,
+  },
 
-    {
-        heading: "DnD 5e Character Race App",
-        image: "./assets/dnd-app.png",
-        mobileImg: "./assets/dnd-app-mobile.png",
-        tech: "React.js, CSS",
-        liveURL: "https://wondrous-fudge-759bc2.netlify.app/",
-        repoURL: "https://github.com/parradaniela/dnd-5e-character-race-app",
-        desc: "React.js App that allows the user to select a standard race from the 5th edition of Dungeons and Dragons using a dropdown menu, then displays information about that race by fetching data from a REST API to populate a number of tabs."
-    },
+  {
+    heading: "DnD 5e Character Race App",
+    image: "./assets/dnd-app.png",
+    mobileImg: "./assets/dnd-app-mobile.png",
+    tech: "React.js, CSS",
+    liveURL: "https://wondrous-fudge-759bc2.netlify.app/",
+    repoURL: "https://github.com/parradaniela/dnd-5e-character-race-app",
+    desc: "React.js App that allows the user to select a standard race from the 5th edition of Dungeons and Dragons using a dropdown menu, then displays information about that race by fetching data from a REST API to populate a number of tabs.",
+  },
 
+  {
+    heading: "Sanctuary Helpers",
+    image: "./assets/sanctuary-helpers.png",
+    mobileImg: "./assets/sanctuary-helpers-mobile.png",
+    tech: "HTML, SCSS, ES6",
+    liveURL: "https://guileless-bonbon-c2a588.netlify.app",
+    repoURL: "https://github.com/parradaniela/sanctuary-helpers",
+    desc: `Multi-page, responsive, accessible PSD conversion following desktop-first design. The original design files can  be viewed <a href="https://drive.google.com/drive/folders/1OcFeadXvm2a0Wr2ZjoA_mDNScxWVZDOv?usp=sharing">here</a>`,
+  },
 
-    {
-        heading: "Sanctuary Helpers",
-        image: "./assets/sanctuary-helpers.png",
-        mobileImg: "./assets/sanctuary-helpers-mobile.png",
-        tech: "HTML, SCSS, ES6",
-        liveURL: "https://guileless-bonbon-c2a588.netlify.app",
-        repoURL: "https://github.com/parradaniela/sanctuary-helpers",
-        desc: `Multi-page, responsive, accessible PSD conversion following desktop-first design. The original design files can  be viewed <a href="https://drive.google.com/drive/folders/1OcFeadXvm2a0Wr2ZjoA_mDNScxWVZDOv?usp=sharing">here</a>`
-    },
-
-    {
-        heading: "Ontario Weather App",
-        image: "./assets/weather-app.png",
-        mobileImg: "./assets/weather-app-mobile.png",
-        tech: "HTML, SCSS, ES6",
-        liveURL: "https://singular-begonia-bd09cf.netlify.app/",
-        repoURL: "https://github.com/weatherAppJuno/weatherApp",
-        desc: `Vanilla JS application that uses the AccuWeather API to pull current forecast data for Toronto, and any city within Ontario entered by the user. Pair project with <a href="https://radoyko.com/">Radojko Radulovic</a>.`
-    },
-
-    // {
-    //     heading: "FunnyLibs!",
-    //     image: "./assets/funnylibs.png",
-    //     mobileImg: "./assets/funnylibs-mobile.png",
-    //     tech: "React.js, CSS",
-    //     liveURL: "https://superlative-pony-40db95.netlify.app/",
-    //     repoURL: "https://github.com/madLibsClone/FunnyLibs/tree/main",
-    //     desc: `A React.js mad libs app designed together with <a href="https://www.tylergeorge.ca/">Tyler George</a>. The app chooses from three random blocks of text and allows the user to replace some words in the text through a form. Uses React Router to navigate between different pages. Check back for updates, we are working on adding more functionality!`,
-    //     inProgress: true
-    // }
+  {
+    heading: "Ontario Weather App",
+    image: "./assets/weather-app.png",
+    mobileImg: "./assets/weather-app-mobile.png",
+    tech: "HTML, SCSS, ES6",
+    liveURL: "https://singular-begonia-bd09cf.netlify.app/",
+    repoURL: "https://github.com/weatherAppJuno/weatherApp",
+    desc: `Vanilla JS application that uses the AccuWeather API to pull current forecast data for Toronto, and any city within Ontario entered by the user. Pair project with <a href="https://radoyko.com/">Radojko Radulovic</a>.`,
+  },
 ];
